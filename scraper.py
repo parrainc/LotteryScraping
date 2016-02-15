19942486b54293ab5a47863eb7efed33ca658dd2
Carlos Parra <carlos.parra.sbr@gmail.com>
2016-02-15 00:40:57 -0400

    Cleaning up the code

--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+# author: Carlos Parra (Parra Inc)
+# python-version: 3.4.3 (3.x)
+
 import re
 import urllib.request
 from bs4 import BeautifulSoup
@@ -10,46 +13,25 @@ except:
     print("Error opening the url")
     exit()
 
-# for i in range(0,3):
-#     regex = '<div class="content"><ul style="width: 600px;float: left;">' \
-#             '<li>(.+?)</li></ul></div>'
-#
-#     # pattern = re.compile(regex)
-#     numbers = re.findall(b"<li>(.+?)</li>", html)
-#     print(numbers)
 soup = BeautifulSoup(html, "html.parser")
 
-other = soup.find_all('div', class_='content')
 lotteries = soup.select('div.lottery-list div.heading')
 numbers = soup.select('div.lottery-list div.content ul')
 date = soup.select('div.lottery-list div.content div.status')
 
 lotteries_names = [lottery_name.contents[4].strip()
                    for lottery_name in lotteries]
-numbers_dict = dict()
 dates_list = [dates.contents[2].strip() for dates in date]
-
-# for dates in date:
-#    print(dates.contents[2].strip())
+numbers_dict = dict()
 
 increment = 0
-# for names in lotteries:
-#     lotteries_names.append(names.contents[4].strip())
 
 for tag in numbers:
-    #var.append(tag.contents)
-    #numbers_list.append(tag.contents)
-
     numbers_list = re.findall('<li>(.+?)</li>+?', str(tag.contents))
-    #print(i)
-    numbers_dict[lotteries_names[increment]] = {'numbers':numbers_list,
-                                                'date':dates_list[increment]}
+    numbers_dict[lotteries_names[increment]] = {'numbers': numbers_list,
+                                                'date': dates_list[increment]}
     increment = increment + 1
 
-    # print(tag.contents[4].strip())
 
-for k,v in numbers_dict.items():
+for k, v in numbers_dict.items():
     print(k, v)
-
-# for tag in soup.find_all(re.compile("<li>(.+?)</li>")):
-#     print(tag.contents)
